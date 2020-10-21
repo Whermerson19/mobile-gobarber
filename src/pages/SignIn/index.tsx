1c9854765ffda514debdf1fd2df368fb56c80a98
Whermerson19 <whermersonc@gmail.com>
2020-10-21 19:38:27 -0300

    Input & Button

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Image, Text } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+    return (
+        <Container>
+            <Image source={logoImg} />
+            <Title>Faça seu Login</Title>
+
+            <Input name='email' icon='mail' placeholder='E-mail' />
+            <Input name='password' icon='lock' placeholder='Senha' />
+
+            <Button onPress={() => {console.log('click')}} >Entrar</Button>
+        </Container>
+    )
+}
+
+export default SignIn;
