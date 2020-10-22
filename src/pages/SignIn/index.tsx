de6de366d9f6971e601c00da4f07e52c970b43d5
Whermerson19 <whermersonc@gmail.com>
2020-10-22 09:15:10 -0300

    Itengrando Unform

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+import { Form } from '@unform/mobile';
 
 import { Container,
          Title, 
@@ -16,10 +18,16 @@ import { Container,
 
 import logoImg from '../../assets/logo.png';
 import { useNavigation } from '@react-navigation/native';
+import { FormHandles } from '@unform/core';
 
 const SignIn: React.FC = () => {
 
+    const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
+
+    const handleSubmit = useCallback((data: object) => {
+        console.log(data);
+    }, []);
 
     return (
         <KeyboardAvoidingView 
@@ -35,10 +43,14 @@ const SignIn: React.FC = () => {
                     <Image source={logoImg} />
                     <Title>Faça seu Login</Title>
 
-                    <Input name='email' icon='mail' placeholder='E-mail' />
-                    <Input name='password' icon='lock' placeholder='Password' />
+                    <Form ref={formRef} onSubmit={handleSubmit} >
 
-                    <Button onPress={() => {console.log('click')}} >Entrar</Button>
+                        <Input name='email' icon='mail' placeholder='E-mail' />
+                        <Input name='password' icon='lock' placeholder='Password' />
+
+                        <Button onPress={() => formRef.current?.submitForm()} >Entrar</Button>
+
+                    </Form>
 
                     <ForgotPassword>
                         <ForgotPasswordText>Reset Passoword</ForgotPasswordText>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
@@ -15,8 +15,12 @@ import { Container,
     } from './styles';
 
 import logoImg from '../../assets/logo.png';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 const SignUp: React.FC = () => {
+
+    const formRef = useRef<FormHandles>(null);
 
     const navigation = useNavigation();
 
@@ -34,11 +38,15 @@ const SignUp: React.FC = () => {
                     <Image source={logoImg} />
                     <Title>Sign Up</Title>
 
+                    <Form ref={formRef} onSubmit={(data) => {console.log(data)}} >
+
                     <Input name='nome' icon='user' placeholder='Name' />
                     <Input name='email' icon='mail' placeholder='E-mail' />
                     <Input name='password' icon='lock' placeholder='Password' />
 
-                    <Button onPress={() => {console.log('click')}} >Entrar</Button>
+                    <Button onPress={() => {formRef.current?.submitForm()}} >Register</Button>
+
+                    </Form>
 
                 </Container>
 
